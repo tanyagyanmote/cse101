8200e148356f65a167f36309340f6ed6a8ad16d8
tanya g <tanya@tanyas-MacBook-Pro.local>
2023-03-09 16:47:19 -0800

    removing comments

--- pa7/Dictionary.cpp
+++ pa7/Dictionary.cpp
@@ -5,34 +5,6 @@
 
 //using mikes puesdocode 
 
-// Dictionary::Node::Node(keyType k, valType v){
-//    set key to k, val to v, left & right & parent nodes to null pointers
-// }
-
-// Dictionary::Dictionary(){
-//    set nil to a new Node("someLongAssStringHere", 999) // key shouldn't be something in the dictionary
-//    set the root to nil
-//    set current nil
-//    set num_pairs to 0
-// }
-
-// // Copy constructor.
-// Dictionary::Dictionary(const Dictionary& D){
-//    // create an empty Dictionary
-//    set nil to a new Node("someLongAssStringHere", 999) // key shouldn't be something in the dictionary
-//    set the root to nil
-//    set current nil
-//    set num_pairs to 0
-//    pre order copy the pairs of D (root of D, nil of D) into this Dictionary 
-
-// }
-
-// // Destructor
-// Dictionary::~Dictionary(){
-//    post order delete on the root node
-//    delete nil
-// }
-
 Dictionary::Node::Node(keyType k, valType v) {
 	key = k;
 	val = v;
@@ -60,4 +32,3 @@ Dictionary::~Dictionary(){
 	postOrderDelete(root);
 	delete nil;
 }
-
